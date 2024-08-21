daf215b810062a30e5d0b1d95367838d6052ef07
CARTOON <brianngugi224@gmail.com>
2024-08-21 17:22:32 +0300

    Update home page with introduction and spinner component

--- app/(main)/_components/home.tsx
+++ app/(main)/_components/home.tsx
@@ -1,7 +1,19 @@
 export const HomePage = () => {
     return (
-        <div>
-            Home
+        <div className="max-w-3xl space-y-4">
+            <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
+            My name is <span className="underline">Brian Kamau</span>
+            </h1>
+            <h3 className="text-base sm:text-sm md:text-2xl font-small">
+            As a DevSecOps engineer, I specialize in integrating robust security practices into the DevOps pipeline. 
+            With a diverse technical skillset encompassing NextJS, GitHub, MongoDB, MySQL, TailwindCSS, Python, Flask, 
+            JavaScript, NodeJS, and HTML, I excel at automating security measures, implementing advanced protocols, 
+            and seamlessly integrating third-party services to enhance functionality and streamline workflows. 
+            My expertise in web design ensures that I create not only secure but also visually appealing and user-friendly interfaces. 
+            My comprehensive approach ensures the creation of secure, efficient, and scalable infrastructures that support agile and 
+            effective software delivery. Committed to protecting organizations from cyber threats, I leverage my skills in both security 
+            and web design to enhance system integrity while meeting modern development demands.
+            </h3>
         </div>
     )
 }
