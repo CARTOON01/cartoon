55d2afc0b4b80c153e3d58c205c2804a143c1eeb
CARTOON <brianngugi224@gmail.com>
2024-08-22 12:51:06 +0300

    Add vercel analytics

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,6 +1,7 @@
 
 import { Header } from "./_components/header";
 import { HomePage } from "./_components/home";
+import { Analytics } from "@vercel/analytics/react"
 
 const Home = () => {
   return ( 
@@ -9,6 +10,7 @@ const Home = () => {
       <main className="h-full pt-40">
         <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 py-10">
           <HomePage />
+          <Analytics />
         </div>
       </main>
     </div>
