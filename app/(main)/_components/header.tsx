2097b8cbc7044c2d91e7295b7a754e67074547af
CARTOON <brianngugi224@gmail.com>
2024-09-04 03:18:00 +0300

    Hot fix

--- app/(main)/_components/header.tsx
+++ app/(main)/_components/header.tsx
@@ -1,18 +1,13 @@
-import { cn } from "@/lib/utils";
+import React from "react";
 import { Logo } from "./logo";
 import { MenuButton } from "./menu-button";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import { useScrolled } from "@/hooks/scroll";
 
 export const Header = () => {
-    const scrolled = useScrolled();
 
     return (
-        <div className={cn(
-            "z-50 fixed top-0 w-full transition-all duration-300",
-            scrolled ? "bg-background/80 backdrop-blur-sm" : "bg-background"
-        )}>
+        <div className="z-50 fixed top-0 w-full transition-all duration-300">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex items-center justify-between h-16">
                     <Logo />
